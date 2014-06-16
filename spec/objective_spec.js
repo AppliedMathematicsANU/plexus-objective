45b9858539623f7f08184a74d653fab9990c294e
odf <olaf.delgado@gmail.com>
2014-06-16 15:24:37 +1000

    refactor check functions in tests

--- spec/objective_spec.js
+++ spec/objective_spec.js
@@ -19,7 +19,7 @@ var genList = function(genElement) {
 
 var shrinkList = function(shrinkElement) {
   return function(list) {
-    var result = []
+    var result = [];
     var i, j, shrunk, tmp;
 
     for (i in list) {
@@ -29,7 +29,7 @@ var shrinkList = function(shrinkElement) {
     }
 
     for (i in list) {
-      tmp = shrinkElement(shrunk[i]);
+      tmp = shrinkElement(list[i]);
       for (j in tmp) {
         shrunk = list.slice();
         shrunk[i] = tmp[j];
@@ -61,46 +61,111 @@ var shrinkKey = function(s) {
 };
 
 
+var genObj = function(genKey, genVal) {
+  return function(n) {
+    var size = comfy.randomInt(0, n);
+    var obj = {};
+    for (var i = 0; i < size; ++i)
+      obj[genKey(n)] = genVal(n);
+    return obj;
+  };
+};
+
+
+var modified = function(obj, key, val) {
+  var result = {};
+  for (var k in obj) {
+    if (k !== key)
+      result[k] = obj[k];
+    else if (val !== undefined)
+      result[k] = val;
+  }
+  return result;
+};
+
+
+var shrinkObj = function(n) {
+  return function(obj) {
+    var result = {};
+    var k, j, tmp;
+
+    for (k in Object.keys(obj))
+      result.push(modified(obj, k));
+
+    for (k in Object.keys(obj)) {
+      tmp = shrinkElement(obj[k]);
+      for (j in tmp)
+        result.push(modified(obj, k, tmp[j]));
+    }
+
+    return result;    
+  };
+};
+
+
+var verifyKeys = function(expected, obj) {
+  var keys = Object.keys(obj);
+  var seen = {};
+  var i, k;
+
+  for (i in keys) {
+    k = keys[i];
+    seen[k] = true;
+    if (!expected[k])
+      return comfy.failure('unexpected key ' + k + ' in result');
+  }
+
+  for (k in expected)
+    if (!seen[k])
+      return comfy.failure('missing key ' + k + ' in result');
+
+  return comfy.success();
+};
+
+
+var verifyValues = function(expected, obj) {
+  var i, key, val;
+
+  for (i in expected) {
+    key = expected[i][0];
+    val = expected[i][1];
+
+    if (obj[key] !== val)
+      return comfy.failure('expected ' + val + ' at position ' + key +
+                           ', got ' + obj[key]);
+  }
+  return comfy.success();
+};
+
+
 describe('the object function', function() {
   var checkKeys = function(list) {
     var obj = ou.object.apply(null, list);
-    var keys = Object.keys(obj);
     var expected = {};
-    var seen = {};
-    var i, k;
+    var i;
 
     for (i = 0; i+1 < list.length; i += 2)
       expected[list[i]] = true;
 
-    for (i in keys)
-      seen[keys[i]] = true;
-
-    for (k in expected)
-      if (!seen[k])
-        return comfy.failure('missing key ' + k + ' in result');
-
-    for (k in seen)
-      if (!expected[k])
-        return comfy.failure('unexpected key ' + k + ' in result');
-
-    return comfy.success();
+    return verifyKeys(expected, obj);
   };
 
   var checkValues = function(list) {
     var obj = ou.object.apply(null, list);
+    var expected = [];
     var seen = {};
-    var key, val;
+    var i, key, val;
 
-    for (var i = Math.floor((list.length - 2) / 2); i >= 0; --i) {
+    for (i = Math.floor((list.length - 2) / 2); i >= 0; --i) {
       key = list[2*i];
       val = list[2*i + 1];
 
-      if (!seen[key] && obj[key] !== val)
-        return comfy.failure('expected ' + val + ' at position ' + key +
-                             ', got ' + obj[key]);
+      if (!seen[key])
+        expected.push([key, val]);
       seen[key] = true;
     }
-    return comfy.success();
+
+    return verifyValues(expected, obj);
   };
 
   describe('when given only integer arguments', function() {
